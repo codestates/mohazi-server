6acd48a0739f4f21ba1c2cfe1e9c99147c8ade1f
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2021-06-29 01:57:09 +0900

    done_rest items

--- routes/selections.js
+++ routes/selections.js
@@ -5,8 +5,8 @@ const { selectionsController } = require('../controllers');
 
 router.get("/cafelists", selectionsController.CafeLists);
 router.get("/parklists", selectionsController.ParkLists);
-// router.get("/exhibitionlists", selectionsController.ExhibitionLists);
-// router.get("/malllists", selectionsController.MallLists);
+router.get("/exhibitionlists", selectionsController.ExhibitionLists);
+router.get("/malllists", selectionsController.MallLists);
 router.get("/restaurantlists", selectionsController.RestaurantLists);
 // router.patch("/selectionupdate", selectionsController.SelectionUpdate.patch);
 
