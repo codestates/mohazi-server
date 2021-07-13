ec41d93c21822c0a8b49754cac14d2900045deed
Franchesca Kim <franchescakim09@gmail.com>
2021-07-13 14:52:54 +0900

    [fix] UserUpdate controller

--- controllers/users/UserUpdate.js
+++ controllers/users/UserUpdate.js
@@ -3,17 +3,17 @@ const s3 = require('../../config/s3');
 
 module.exports = {
   put: async (req, res) => {
-    const { UserId, Email, Username, Password, Photo, Description } = req.body
+    const { userId, username, password, photo, description } = req.body
     // console.log(req.session)
 
-    console.log(req.body);
+    //console.log(req.body);
     // console.log(req.body);
     //console.log('여기',req.file.filename);
     // console.log(UserId);
-    console.log('req. file:', req.file.key)
+    //console.log('req. file:', req.body.photo)
 
     await user
-      .findOne({ where: { id: UserId } })
+      .findOne({ where: { id: userId } })
       .then(res => {
         const prevPhoto = res.dataValues.photo;
         
@@ -22,33 +22,34 @@ module.exports = {
           Key: prevPhoto
         }, function(err, data){
           if(data) {
-            console.log(data)
+            console.log('delete success', data)
           } else {
             console.log(err)
           }
         });
-      });
-
-    await user.update({
-      username: Username,
-      password: Password,
-      photo: req.file.key,
-      description: Description,
-    }, {
-      where: {
-        id: UserId
-      }
-    }).then(userInfo => {
-
-      // console.log(userInfo);
-      return res.status(200).send({
-        message: "성공적으로 정보를 바꾸었습니다.",
       })
-    })
-      .catch(err => {
-        res.status(400).send({
-          err: "err"
+      .then(response => {
+        user.update({
+          username: username,
+          password: password,
+          photo: photo,
+          description: description,
+        }, {
+          where: {
+            id: userId
+          }
+        }).then(userInfo => {
+    
+          console.log(userInfo);
+          return res.status(200).send({
+            message: "성공적으로 정보를 바꾸었습니다.",
+          })
         })
+          .catch(err => {
+            res.status(400).send({
+              err: "err"
+            })
+          })
       })
   }
 }
