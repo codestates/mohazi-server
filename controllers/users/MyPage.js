b520b156025b41799b82fd8ac25db44b19bd4986
hoon <theji@kakao.com>
2021-07-14 13:40:52 +0900

    deploy5

---
+++ controllers/users/MyPage.js
@@ -0,0 +1,44 @@
+// /mypage에 들어갔을 때 필요한 정보들
+// user정보, 해당 user가 갖고 있는 카드들, 해당 user가 tag된 카드들
+// user table, user_daily table
+const { user, dailyCard } = require("../../models");
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const { userId } = req.body;
+
+  await user
+    .findOne({
+      where: {
+        id: userId,
+      },
+    })
+    .then((admin) => {
+      dailyCard
+        .findAll({
+          raw: true,
+          where: {
+            admin: admin.id,
+          },
+        })
+        .then((cards) => {
+          axios
+            .get(`https://api.mohazi.site/searchtaggedcards`, {
+              params: {
+                userId: userId,
+              },
+            })
+            .then((tagged) => {
+              res.status(200).send({
+                message: `${userId}번 유저의 정보를 조회했습니다.`,
+                userInfo: admin,
+                myCards: cards,
+                taggedCards: tagged.data.taggedCards,
+              });
+            });
+        });
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "유저 정보를 조회할 수 없습니다." });
+    });
+};
---
+++ controllers/users/SearchFriends.js
@@ -0,0 +1,46 @@
+// request
+// 1. userId
+// 2. dailyCardId
+const { user, user_daily, dailyCard, selection } = require("../../models");
+const { Op } = require("sequelize");
+
+module.exports = async (req, res) => {
+  const { dailyCardId } = req.query;
+  // console.log(req.query)
+
+  try {
+    const friends = await user_daily.findAll({
+      raw: true,
+      where: {
+        dailyCards_id: dailyCardId,
+      },
+    });
+    // console.log(friends);
+
+    const userIDs = friends.map((el) => el.user_id);
+    // console.log(userIDs)
+    if (userIDs.length === 0) {
+      return res
+        .status(400)
+        .send({ message: "친구 목록을 불러올 수 없습니다." });
+    }
+
+    const friendsInfo = await user.findAll({
+      raw: true,
+      where: {
+        id: {
+          [Op.in]: userIDs,
+        },
+      },
+    });
+    // console.log(friendsInfo);
+
+    res.status(200).send({
+      message: `${dailyCardId}번 카드에 속한 유저 목록입니다.`,
+      friendsInfo,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
