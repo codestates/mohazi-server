943da19bd409db858ce1f7d4c5938906f5e1c457
JungHyun Jo <wjstkwhwjdgus@gmail.com>
2021-07-16 10:49:27 +0900

    Fix SocialLogin duplication error

--- controllers/users/SocialLogin.js
+++ controllers/users/SocialLogin.js
@@ -1,25 +1,25 @@
 const { user } = require("../../models");
 
 // 이미 존재하는 이메일도 비밀번호 변경 시 다시 로그아웃 후 소셜로그인 할 때 DB에 새로 저장됨
-// 1. 
-
+// 1. 중복된 이메일이 존재 시 /signup 엔드포인트로 가지 않도록 -> 이미 돼있다?
+// 2. social 계정일 경우, 비밀번호 수정이 불가능하도록?
 
 module.exports = {
   post: async (req, res) => {
+    const { email, password, name } = req.body;
     await user
       .findOne({
         where: {
-          email: req.body.email,
-          password: req.body.password,
+          email: email,
         },
       })
       .then((userInfo) => {
         if (!userInfo) {
           user
             .create({
-              email: req.body.email,
-              password: req.body.password,
-              username: req.body.name,
+              email: email,
+              password: password,
+              username: name,
             })
             .then((socialInfo) => {
               req.session.save(function () {
